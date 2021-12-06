ddfb7527d3c7c58f3042478fd21278f45a6013a6
Tom Shafer <tj@tjshafer.com>
2021-12-06 12:09:09 -0700

    bug fixes

--- resources/views/livewire/forms-results.blade.php
+++ resources/views/livewire/forms-results.blade.php
@@ -1,18 +1,18 @@
 
 <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <h2 class="text-xl font-semibold leading-tight text-gray-800">
         {{ $form->name }}
     </h2>
 </x-slot>
 
 <div>
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="py-10 mx-auto max-w-7xl sm:px-6 lg:px-8">
          <div>
-            <table class="w-1/2 border-collapse m-auto">
+            <table class="w-1/2 m-auto border-collapse">
                 <thead class="table-auto h-[278px]">
                     <tr>
                         <th scope="col" class="align-bottom">GB Member</th>
-                        <th scope="col" class="bg-blue-500 border w-10 whitespace-nowrap">
+                        <th scope="col" class="w-10 bg-blue-500 border whitespace-nowrap">
                             <div style="transform: translate(0, 115px) rotate(270deg);" class="w-10">
                                 <span class="p-2 text-white ">PROGRESS METRIC</span>
                             </div>
@@ -21,7 +21,7 @@
                             $questions = collect($questions)->reject(fn($item) => $item['section'] == 'custom');
                         @endphp
                         @foreach($questions as $question)
-                            <th scope="col" class="bg-white border w-10 whitespace-nowrap">
+                            <th scope="col" class="w-10 bg-white border whitespace-nowrap">
                                 <div style="transform: translate(0, 115px) rotate(270deg);" class="w-10">
                                     <span class="text-{{ $question['color'] }} p-2">{{ $question['question'] }}</span>
                                 </div>
@@ -33,7 +33,7 @@
                         @endphp
 
                         @foreach($sections->all() as $section => $sectionData)
-                            <th scope="col" class="bg-white border w-10 whitespace-nowrap">
+                            <th scope="col" class="w-10 bg-white border whitespace-nowrap">
                                 <div style="transform: translate(0, 115px) rotate(270deg);" class="w-10">
                                     <span class="text-{{ $sectionData->first()['color'] }} p-2">{{ implode(' ', explode('_', $section)) }}</span>
                                 </div>
@@ -77,10 +77,10 @@
                             @php
                                 $progressMetricTotal += number_format($total / $totalSections, 1);
                             @endphp
-                            <td class="border-2 text-right bg-white p-2">{{ number_format($total / $totalSections, 1) }}</td>
+                            <td class="p-2 text-right bg-white border-2">{{ number_format($total / $totalSections, 1) }}</td>
 
                             @foreach($questions as $question)
-                                <td class="border-2 text-center bg-white p-2">{{ Arr::get($response->questions, Str::slug($question['question'])) }}</td>
+                                <td class="p-2 text-center bg-white border-2">{{ Arr::get($response->questions, Str::slug($question['question'])) }}</td>
                             @endforeach
 
                             @php
@@ -88,7 +88,7 @@
                             @endphp
 
                             @foreach($sections->all() as $section => $sectionData)
-                                <td class="border-2 text-center bg-white p-2">
+                                <td class="p-2 text-center bg-white border-2">
                                     @php
                                         $sectionQuestions = $sectionData->pluck('question')->map(fn($item) => Str::slug($item))->toArray();
                                         $total = collect($response->questions)->filter(function($item, $key) use($sectionQuestions) {
@@ -106,25 +106,32 @@
 
                     <tr>
                         <td></td>
-                        <td class="border-2 bg-blue-500 text-white font-bold text-right p-2">
-                            {{  number_format($progressMetricTotal / $this->form->responses->count(), 1) }}
+                        <td class="p-2 font-bold text-right text-white bg-blue-500 border-2">
+                            @if($progressMetricTotal > 0)
+                                {{ number_format($progressMetricTotal / $this->form->responses->count(), 1) }}
+                            @endif
                         </td>
 
                         @foreach($questions as $question)
-                            <td class="border-2 text-center bg-blue-100 p-2">
+                            <td class="p-2 text-center bg-blue-100 border-2">
                                 @php
-                                $mappedQuestions = collect($this->form->responses)->map(function ($value) {
-                                    return $value->response['questions'];
-                                });
+                                    $mappedQuestions = collect($this->form->responses)->map(function ($value) {
+                                        return $value->response['questions'];
+                                    });
 
-                                echo number_format( $mappedQuestions->pluck(Str::slug($question['question']))->sum() / $mappedQuestions->count(), 1);
+                                    if( $mappedQuestions->pluck(Str::slug($question['question']))->sum() > 0) {
+                                        echo number_format( $mappedQuestions->pluck(Str::slug($question['question']))->sum() / $mappedQuestions->count(), 1);
+                                    }
+
                                 @endphp
                             </td>
                         @endforeach
 
                         @foreach($sections->all() as $section => $sectionData)
                             <td class="bg-{{ $sectionData->first()['color'] }} border-2 text-center p-2">
-                                {{ number_format($sectiontotals[$section .'_count'] / $this->form->responses->count(), 1) }}
+                                @if($sectiontotals[$section .'_count'])
+                                    {{ number_format($sectiontotals[$section .'_count'] / $this->form->responses->count(), 1) }}
+                                @endif
                             </td>
                         @endforeach
 
@@ -132,14 +139,14 @@
                 </tbody>
             </table>
 
-           {{--  <table class="w-full border-collapse m-auto mt-10">
+           {{--  <table class="w-full m-auto mt-10 border-collapse">
                 <thead class="table-auto">
                     <tr>
                         <th scope="col"></th>
                         @foreach($feedback_questions as $question)
-                            <th scope="col" class="text-left py-2 px-4">
-                                <div class="text-lg mb-2">{{ $question['question'] }}</div>
-                                <div class="font-normal text-xs">{{ $question['description'] }}</div>
+                            <th scope="col" class="px-4 py-2 text-left">
+                                <div class="mb-2 text-lg">{{ $question['question'] }}</div>
+                                <div class="text-xs font-normal">{{ $question['description'] }}</div>
                             </th>
                         @endforeach
                     </tr>
@@ -148,27 +155,30 @@
                 <tbody>
                     @foreach($this->form->responses as $response)
                     <tr class="border-b-2 border-black border-solid">
-                        <td class="py-2 px-4">{{ $response->email }} </td>
+                        <td class="px-4 py-2">{{ $response->email }} </td>
                         @foreach($response->response['questions']['custom'] as $custom)
-                            <td class="py-2 px-4"> {{ $custom }} </td>
+                            <td class="px-4 py-2"> {{ $custom }} </td>
                         @endforeach
                     </tr>
                     @endforeach
                     <tr>
-                        <td class="text-sm italic p-2" colspan="{{ count($feedback_questions) + 1 }}">*Note: All Scores are calculated based on the last Evaluation, they are not a aggregate of all Progress scores.</td>
+                        <td class="p-2 text-sm italic" colspan="{{ count($feedback_questions) + 1 }}">*Note: All Scores are calculated based on the last Evaluation, they are not a aggregate of all Progress scores.</td>
                     </tr>
                 </tbody>
             </table> --}}
 
             <div class="w-2/3 m-auto mt-10">
-                <div class="grid grid-rows- grid-flow-col gap-2">
+                <div class="grid grid-flow-col gap-2 grid-rows-">
 
                      @foreach($questions->where('hidden', false)->sortBy('order')->take(7) as $question)
                         @php
                             $mappedQuestions = collect($this->form->responses)->map(function ($value) {
                                 return $value->response['questions'];
                             });
-                            $number = number_format( $mappedQuestions->pluck(Str::slug($question['question']))->sum() / $mappedQuestions->count(), 1);
+                            $number = 0;
+                            if($mappedQuestions->pluck(Str::slug($question['question']))->sum() > 0) {
+                                $number = number_format( $mappedQuestions->pluck(Str::slug($question['question']))->sum() / $mappedQuestions->count(), 1);
+                            }
                         @endphp
 
                         <div class="{{ $question['classes']}} {{ colorize($number) }} flex items-center justify-center">{{ $question['question'] }}<br/>{{ $number}}</div>
@@ -176,13 +186,16 @@
                 </div>
 
 
-                <div class="grid grid-rows- grid-flow-col gap-2 mt-2">
+                <div class="grid grid-flow-col gap-2 mt-2 grid-rows-">
                      @foreach($questions->where('hidden', false)->sortBy('order')->skip(7)->take(2) as $question)
                         @php
                             $mappedQuestions = collect($this->form->responses)->map(function ($value) {
                                 return $value->response['questions'];
                             });
-                            $number = number_format( $mappedQuestions->pluck(Str::slug($question['question']))->sum() / $mappedQuestions->count(), 1);
+                            $number = 0;
+                            if($mappedQuestions->pluck(Str::slug($question['question']))->sum()) {
+                                $number = number_format( $mappedQuestions->pluck(Str::slug($question['question']))->sum() / $mappedQuestions->count(), 1);
+                            }
                         @endphp
 
                         <div class="{{ $question['classes']}} {{ colorize($number) }} flex items-center justify-center">{{ $question['question'] }}<br/>{{ $number}}</div>
