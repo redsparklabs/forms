8c8d94d63bc5b3b81b35241fbf27d32de754f97f
Tom Shafer <tj@tjshafer.com>
2022-02-14 15:18:36 -0700

    fixings

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -28,7 +28,7 @@
             </x-slot>
 
             <x-slot name="description">
-                {{ __('All of the projects that are part of this Assessment Board.') }}
+                {{ __('All of the projects that are part of this Assessment.') }}
             </x-slot>
 
             <x-slot name="content">
